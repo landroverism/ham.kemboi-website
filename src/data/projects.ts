dbc8e0d28b252ca786bc7bc3ee9696ba4a40a1e1
volkswagen <landroverism@kali.hamkemboi>
2025-04-24 01:09:55 +0300

    sorted links

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,13 +1,6 @@
 
 export const projects = [
-  {
-    title: "Ireporter",
-    description: "iReporter is an app that allows users to report incidents like crimes or community issues by capturing and uploading photos, videos, and descriptions in real-time, helping authorities address problems quickly.",
-    image: "/images/webcapture.jpeg",
-    tech: ["HTML5", "CSS3 | SCSS", "React | TypeScript", "Python (Flask)"],
-    github: "https://github.com/landroverism/ireporter-new.git",
-    live: "https://ireporter-new.vercel.app/"
-  },
+
   {
     title: "Seamless Web App",
     description: "Seamless is a web platform designed to modernize the custom tailoring industry by enabling remote ordering, self-measurements, and direct communication between clients and tailors.",
@@ -15,5 +8,13 @@ export const projects = [
     tech: ["Vue", "HTML5", "CSS3 | SCSS", "Typescript"],
     github: "https://github.com/landroverism/SEAMLESS-web-app.git",
     live: "https://seam-less-app.netlify.app/"
+  },
+  {
+    title: "Ireporter",
+    description: "iReporter is an app that allows users to report incidents like crimes or community issues by capturing and uploading photos, videos, and descriptions in real-time, helping authorities address problems quickly.",
+    image: "/images/webcapture.jpeg",
+    tech: ["HTML5", "CSS3 | SCSS", "React | TypeScript", "Python (Flask)"],
+    github: "https://github.com/landroverism/ireporter-new.git",
+    live: "https://ireporter-new.vercel.app/"
   }
 ];
