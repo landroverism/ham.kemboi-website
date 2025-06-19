0de6aeb9ec082f207512c41d07bf99b14b77b40d
landroverism <kemboiham3@gmail.com>
2025-06-19 10:46:45 +0300

    added hamwood

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -8,6 +8,14 @@ export const projects = [
     live: "https://tailorly.netlify.app/"
   },
   {
+    title: "hamWood",
+    description: "hamWood crafts premium, bespoke furniture where artisanal craftsmanship meets modern elegance. Each handcrafted piece combines rich amber and emerald aesthetics with sustainable materials, transforming ordinary spaces into extraordinary experiences. Not just furniture—we create tomorrow's heirlooms",
+    image: "/images/hamwood.png",
+    tech: ["Next.js 15.1.-", "React | Material UI Icons", "TypeScript", "Turbopack", "Supabase", "Tailwind CSS", "Socket.io", "Socket.io-redis"],
+    github: "https://github.com/landroverism/hamwood.git",
+    live: "https:hamwood.netlify.app/"
+  },
+  {
     title: "Ireporter",
     description: "iReporter is an app that allows users to report incidents like crimes or community issues by capturing and uploading photos, videos, and descriptions in real-time, helping authorities address problems quickly.",
     image: "/images/webcapture.jpeg",
@@ -15,4 +23,5 @@ export const projects = [
     github: "https://github.com/landroverism/ireporter-new.git",
     live: "https://ireporter-new.vercel.app/"
   }
+
 ];
