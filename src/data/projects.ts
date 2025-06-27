1ea05dbf6ccd7c9c1ce9e5ec43e3de8594d3424a
landroverism <kemboiham3@gmail.com>
2025-06-27 19:40:04 +0300

    fixed tech

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -9,6 +9,15 @@ export const projects = [
     live: "https://ireporter-new.netlify.app/"
   },
   
+
+  {
+    title: "hamWood",
+    description: "hamWood crafts premium, bespoke furniture where artisanal craftsmanship meets modern elegance. Each handcrafted piece combines rich amber and emerald aesthetics with sustainable materials, transforming ordinary spaces into extraordinary experiences. Not just furniture—we create tomorrow's heirlooms",
+    image: "/images/hamwood.png",
+    tech: ["Next.js 15.1.-", "React | Material UI Icons", "TypeScript", "Turbopack", "Supabase", "Tailwind CSS", "Socket.io", "Socket.io-redis"],
+    github: "https://github.com/landroverism/hamwood.git",
+    live: "https://hamwood.netlify.app"
+  },
   {
     title: "Tailorly",
     description: "Tailorly is a web platform designed to modernize the custom tailoring industry by enabling remote ordering, self-measurements, and direct communication between clients and tailors.",
@@ -17,14 +26,6 @@ export const projects = [
     github: "https://github.com/landroverism/Tailorly#",
     live: "https://tailorly.netlify.app/"
   },
-  {
-    title: "hamWood",
-    description: "hamWood crafts premium, bespoke furniture where artisanal craftsmanship meets modern elegance. Each handcrafted piece combines rich amber and emerald aesthetics with sustainable materials, transforming ordinary spaces into extraordinary experiences. Not just furniture—we create tomorrow's heirlooms",
-    image: "/images/hamwood.png",
-    tech: ["Next.js 15.1.-", "React | Material UI Icons", "TypeScript", "Turbopack", "Supabase", "Tailwind CSS", "Socket.io", "Socket.io-redis"],
-    github: "https://github.com/landroverism/hamwood.git",
-    live: "https://hamwood.netlify.app"
-  }
 
 
 ];
