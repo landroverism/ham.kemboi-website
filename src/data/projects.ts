27e82c769ac92e55a2fbb0a558019a9a3d446d71
hamkemboi <hamkemboi@localhost.localdomain>
2025-08-31 11:48:43 +0300

    fixed live link

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,7 +3,7 @@ export const projects = [
   {
     title: "Ireporter",
     description: "iReporter is a cutting-edge web platform that empowers citizens to report corruption, request government intervention, and highlight issues requiring attention. Built with modern web technologies and enhanced with AI capabilities, this application serves as a digital bridge between citizens and authorities.",
-    image: "/images/Reporter.png",
+        image: "/images/Ireporter-pic.jpeg",
     tech: ["HTML5", "CSS3 | SCSS", "React 19 | TypeScript", "Convex", "Node(Express)", "gpt-4.1-nano(AI smart assistant)", "Tailwind CSS", "Socket.io", "Socket.io-redis"],
     github: "https://github.com/landroverism/iReporter-Application",
     live: "https://ireporter-new.netlify.app/",
@@ -11,14 +11,14 @@ export const projects = [
   },
   
 
-  {
-    title: "hamWood",
-    description: "hamWood crafts premium, bespoke furniture where artisanal craftsmanship meets modern elegance. Each handcrafted piece combines rich amber and emerald aesthetics with sustainable materials, transforming ordinary spaces into extraordinary experiences. Not just furniture—we create tomorrow's heirlooms",
-    image: "/images/hamwood.png",
-    tech: ["Next.js 15.1.-", "React | Material UI Icons", "TypeScript", "Turbopack", "Supabase", "Tailwind CSS", "Socket.io", "Socket.io-redis"],
-    github: "https://github.com/landroverism/hamwood.git",
-    live: "https://hamwood.netlify.app"
-  },
+  // {
+  //   title: "hamWood",
+  //   description: "hamWood crafts premium, bespoke furniture where artisanal craftsmanship meets modern elegance. Each handcrafted piece combines rich amber and emerald aesthetics with sustainable materials, transforming ordinary spaces into extraordinary experiences. Not just furniture—we create tomorrow's heirlooms",
+  //   image: "/images/hamwood.png",
+  //   tech: ["Next.js 15.1.-", "React | Material UI Icons", "TypeScript", "Turbopack", "Supabase", "Tailwind CSS", "Socket.io", "Socket.io-redis"],
+  //   github: "https://github.com/landroverism/hamwood.git",
+  //   live: "https://hamwood.netlify.app"
+  // },
   {
     title: "Tailorly",
     description: "Tailorly is a web platform designed to modernize the custom tailoring industry by enabling remote ordering, self-measurements, and direct communication between clients and tailors.",
